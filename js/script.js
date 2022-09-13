2bd614d7e94e3df8974e8cc626f80f5432201f44
CHRIS HORNE <87401567+Chris-Horne@users.noreply.github.com>
2022-09-12 18:39:18 -0700

    Validated user input

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 
 const word = "magnolia";
+const guessedLetters = []
 
 const placeholder = function (word) {
     const placeholderLetters = [];
@@ -22,6 +23,35 @@ placeholder(word);
 
 guessLetterButton.addEventListener("click", function (e) {
     e.preventDefault();
+    message.innerText = "";
     const guess = letterInput.value;
     console.log(guess);
+    
+    const goodGuess = validateInput(guess);
 })
+
+
+const validateInput = function(input){
+  const acceptedLetter = /[a-zA-Z]/;
+  if (input.length === 0) {
+    message.innerText = "Please enter a letter."
+  } 
+  else if (input.length > 1) {
+    message.innerText = "Please enter only one letter."
+  }
+  else if (!input.match(acceptedLetter)) {
+    message.innerText = "Please enter a letter from A to Z."
+  }
+  else {
+    return input
+  }
+};
+
+const makeGuess = function(guess) {
+  guess = guess.toUppercase();
+  if(guessedLetters.includes(guess)) {
+    message.innerText = "You have already guessed this letter.";
+  } else {
+    guessedLetters.push(guess);
+  }
+}
